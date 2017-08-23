cf741d2318ab9a18ac94059babcf69e2593d2d2e
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-08-23 04:33:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if position_taken?(index) && between?(0, 8)
+  if position_taken?(index) && index.between?(0, 8)
     true
   else
     false
